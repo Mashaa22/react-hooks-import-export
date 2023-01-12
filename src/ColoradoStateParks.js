86707ebfe356eef15b61d9662bc8b4d07a284b6a
AlexMachariaMaranga <amaranga22@gmail.com>
2023-01-12 08:33:52 +0300

    commit

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (<>
+    <h1>Colorado State Parks!</h1>
+    <MesaVerde />
+    <li>
+      {RMFunctions.trees}{RMFunctions.wildlife()}{RMFunctions.elevation()}
+    </li>
+   
+    </>
+  );
 }
+
+export default ColoradoStateParks;
